5b8b262ce4b40772634fd9dc4d3b55374394a1de
vidb <vidya.bhagwat@softwareag.com>
2018-06-25 09:33:14 +0530

    commit

--- MWS/Task UI/OrderToCash80Tasks/src/com/webmethods/caf/taskclient/ImprovedManualCreditCheckRuleContext.java
+++
@@ -1,4 +0,0 @@
-package com.webmethods.caf.taskclient;
-
-public class ImprovedManualCreditCheckRuleContext  extends  com.webmethods.caf.faces.data.task.impl.BaseTaskRuleContext {
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# WM_O2C
-
-this is repo for webMethods O2C project and will have development codes like ISPakages & Designer assets
